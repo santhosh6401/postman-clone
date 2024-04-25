2fd6a717c879f7d717ecdaa3d82ea3c44a3c1d3a
santhoshkumar <161711343+santhoshkumarsh@users.noreply.github.com>
2024-04-25 16:18:54 +0530

    EMS :: initial commit

---
+++ src/main/java/com/eterio/postman/alt/PostmanCloneApplication.java
@@ -0,0 +1,13 @@
+package com.eterio.postman.alt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmanCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmanCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eterio/postman/alt/PostmanCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eterio.postman.alt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmanCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
