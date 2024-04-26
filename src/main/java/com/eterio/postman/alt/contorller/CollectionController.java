06c1a84e198beb8463b66e05ee3c16f4282d24b4
santhoshkumar <161711343+santhoshkumarsh@users.noreply.github.com>
2024-04-26 15:18:27 +0530

    EMS :: mongo collection swagger basis added

---
+++ src/main/java/com/eterio/postman/alt/contorller/CollectionController.java
@@ -0,0 +1,18 @@
+package com.eterio.postman.alt.contorller;
+
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+
+@Api(tags = "Collection Application", value = "Collection Application")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CollectionController {
+
+
+}
+
